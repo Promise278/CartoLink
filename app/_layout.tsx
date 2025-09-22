bc0f4e77171eeceb88081610777d0c4a8466f78a
Promise278 <promiseobi2007@gmail.com>
2025-09-22 17:04:56 +0100

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,9 @@ import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import "./global.css"
 
-import { useColorScheme } from "@/hooks/useColorScheme";
+// import { useColorScheme } from "@/hooks/useColorScheme";
+import { useColorScheme } from "../hooks/use-color-scheme";
+
 
 
 export default function RootLayout() {
